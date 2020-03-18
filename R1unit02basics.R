898d4065fe2e7ef885e4f7ced7b523e4cd8d562d
Felix <13898512+FelixWuYH@users.noreply.github.com>
2020-03-18 15:50:29 +0800

    test
    update

--- R1unit02basics.R
+++ R1unit02basics.R
@@ -1,13 +1,17 @@
+#####
+rm(list=ls())
+###
 # R for Everyone: Advanced Analytics and Graphics Ch01-04.
 # ------------------< 1. basics >-------------------------------
-library(swirl)
-require(swirl)
+library(base)
+require(graphics)
 ?require
 # Details ... require is designed for use inside other functions;
 # it returns FALSE and gives a warning if the package does not exist
 # (rather than an error as library() does by default).
 ??library
-library(swirlx) # Error!
+# library(swirlx)
+#
 require(swirlx) # Warning!
 # body(require)
 # value <- tryCatch(library(package, ...))
@@ -32,17 +36,19 @@ dir()
 sqrt(5^6)
 #
 # == != > <=
-1+2*3 <= 4/5^6
+1+2*3 != 4/5^6
 # 4/5^6
-1:10 < 5
 #
-# <-  =  
+# vectorized
+1:10 < 5
+# <-  =
 x <- (1:10 < 5)
 y = which(x == TRUE)
 y
 # which(1:10 < 5)
 #
 ###
+# variable names can also be data values
 assign("w", 99)
 assign("w[2]", 66)
 ls()
@@ -51,19 +57,26 @@ w[2]   # NA...
 get("w[2]")
 rm(w)
 ls()
+# w[2]
+# num <- 1; get( paste0(letters[23],"[",num+1,"]") )
+#
 # ------------------< 3. singleton data types >------------------------------------
 class(y)
 is.numeric(y)
 typeof(y)
 class(y) <- "define a class by yourself"
 y
-typeof(y) <- "no, you can't"   # Error!
+# typeof(y) <- "no, you can't"
+#
 class(y) <- "integer"
 # y
+# class( as.numeric(y) )
+#
 x
 class(x)
 is.numeric(x)
 is.logical(x)
+# class( c(x, 0L) )
 #
 # y is an integer vector
 # x is a logical vector
@@ -71,35 +84,48 @@ nchar(y)
 length(y)
 nchar(x)
 length(x)
-# nchar(as.logical(y))
-# nchar(as.integer(x))
+# nchar( as.logical(y) )
+# nchar( as.integer(x) )
 #
 ###
 # z is a character vector
-(z <- as.character(y*100))
+( z <- as.character(y*100) )
 class(z)
 nchar(z)
 length(z)
 #
 ###
 # Date is a stored as a double to keep time
-(date <- as.Date("2017-02-24"))
-(d1 <- as.POSIXct(date)) # CST
-(d2 <- as.POSIXlt(date)) # UTC
-typeof(date)
-typeof(d1)
-typeof(d2)
-#
-# three classes: Date, POSIXt/POSIXct, POSIXt/POSIXlt
+( date <- as.Date("2019-03-18") )
+class(date)
 ?Date
 example(Date)
-class(date)
+typeof(date)
+#
+# three classes: Date, POSIXt/POSIXct, POSIXt/POSIXlt
+# Dates are represented as the number of days since 1970-01-01, with negative values
+# for earlier dates. It is intended that the date should be an integer, but this is 
+# not enforced in the internal representation.
+( d1 <- as.POSIXct(date) ) # CST
+( d2 <- as.POSIXlt(date) ) # UTC (GMT)
+typeof(d1)
+typeof(d2)
 class(d1)
 class(d2)
-date == d2   # Warning!
+#
+# Date-Time Classes: POSIXt
+# Class "POSIXct" represents the (signed) number of seconds since the beginning 
+# of 1970 (in the UTC time zone) as a numeric vector. Class "POSIXlt" is a named list
+# of vectors representing sec: 0–61, min: 0–59, hour: 0–23, mday: 1–31, mon: 0–11, 
+# year: years since 1900, wday: 0–6, starting on Sunday, yday 0–365
 d1 == d2
+options(warn = -1)
+date == d1   # Warning! 
+date == d2   # Warning!
+options(warn = 0)
 # format(date) == format(d2)
 # format(d1) == format(d2)
+# ?format
 #
 ### constants: TRUE FALSE NA NULL
 is.logical(x)
@@ -107,15 +133,21 @@ x[c(1,3)] <- NA
 x
 is.na(x)
 sum(x)   # NA...
+#
+# ignore NA
 sum(x, na.rm = TRUE)
-is.null(x)
-pos <- which(is.na(x))
+pos <- which( is.na(x) )
 # pos
 x[-pos]
 sum(x[-pos])
 # x <- x[!is.na(x)]
 #
-# -------------------< 4. multi-valued data types >----------------------------------
+is.null(x)
+c(d2$year,d2$mon,d2$mday)   # mon: 0-11
+d2$mon <- NULL
+# d2
+#
+# ------------------< 4. multi-valued data types >----------------------------------
 # concatenate, sequence
 c(1:3, 4, 5:9)
 seq(5, 9, by=2)
@@ -124,8 +156,9 @@ x
 # seq(5, 6, by=2)
 # seq(5, -5, by=1.5)
 #
-### access by index: [:] [c()]
-(x <- x > 0)
+###
+# access by index: [:] [c()]
+( x <- x > 0 )
 any(x)
 all(x)
 x[2]
@@ -136,31 +169,46 @@ x[c(2,4,8)]   # NA...
 names(x)
 names(x) <- c("1st","2nd", "3rd", "4th")
 x
+str(x)
+# attributes(x)$names
 #
-### factor is an useful data type
+###
+# factor is an useful data type
 as.factor(x)
 class(x)
 typeof(x)
 x.fact <- factor(x)
 x.fact
 #
-# factor is indexed by integers for better efficiency
+# factor is indexed by integers for better efficiency "in common cases"
 class(x.fact)
 typeof(x.fact)
 factor(x, labels = c("Neg", "Pos"))
 # levels(x.fact) <- c("Neg", "Pos")
+# levels(x.fact) <- c(1, 0, -1); sum(x.fact)
 #
-# ------------------------< 5. data.frame >----------------------------------
+# ------------------< practice by yourself >-----------------------
+# Learning on swirl: DataScienceAndR
+# 4: 01-RBasic-03-Data-Structure-Object
+# 5: 01-RBasic-04-Factors
+# 6: 01-RBasic-05-Arrays-Matrices
+#
+# ------------------< 5. data.frame >----------------------------------
 dataf <- data.frame(no = 1:7, name = names(x), check = x, fact = x.fact)
-str(dataf)
+dataf
 class(dataf$check)
 class(dataf$fact)
+class(dataf$name)
+# class( names(x) )
+#
+str(dataf)
 View(dataf)
+# length(dataf)
 ###
 nrow(dataf)
 ncol(dataf)
 dim(dataf)
-(tempf <- data.frame(names(x), x, stringsAsFactors = FALSE))
+( tempf <- data.frame(names(x), x, stringsAsFactors = FALSE) )
 str(tempf)
 # stringAsFactors = TRUE is the default setting,
 # but it can be changed by options(stringsAsFactors = FALSE)
@@ -168,20 +216,23 @@ str(tempf)
 ### Use $ to access a column
 dataf$name
 dataf$name <- as.character(dataf$name)
+# str(dataf)
+# is.na(dataf$name)
 dataf$name[is.na(dataf$name)] <- c("5th", "6th", "7th")
-dataf$name
+dataf$name   # must transform chr into factor before the asssignment
 head(dataf,3)
 tail(dataf,3)
 rownames(dataf) <- dataf$name
 dataf
 #
-# ------------------------< 6. matrix >----------------------------------
+# ------------------< 6. matrix >----------------------------------
 # recycling rule
 mx <- matrix(1:15, nrow =6)
+# options(warn = -1)
 mx
 nrow(mx); ncol(mx); dim(mx)
-#
-# matrix transpose, multiplication, element-wise  
+# length(mx)
+# matrix transpose, matrix multiplication, element-wise mupltiples  
 t(mx)
 mx %*% t(mx)
 mx * mx
@@ -191,55 +242,35 @@ colnames(mx) <- c(3:1)
 mx
 LETTERS[1:3]
 colnames(mx) <- c(LETTERS[3:1])
-mx
+# mx
 letters[c(1:3,23:30)]
-# rownames(mx) <- c(letters[1:nrow(mx)])
+rownames(mx) <- c(letters[1:nrow(mx)])
+# mx
 #
-# ----------------------------< practice by yourself >------------------------------
-### Learning on swirl
-#
-browseURL(url="http://datascienceandr.org/index.html")
-if (!require(swirl)) {
-  install.packages("swirl")
-  library(swirl)
-}
-install_from_swirl("R Programming Alt")
-# install_course_zip("DataScienceAndR.zip")
-# setwd("...") if error 1 in extracting from zip file 
-###
-swirl()
-## DataScienceAndR
-# 4: 01-RBasic-03-Data-Structure-Object
-# 5: 01-RBasic-04-Factors
-# 6: 01-RBasic-05-Arrays-Matrices
-#
-# ------------< EX2. post one of your findings in swirl courses ----------
-###
-# ------------------------< 7. Function >----------------------------------
-#
-say.hello <- function()
-{
+# ------------------< 7. Function >----------------------------------
+say.hello <- function() {
   print("Hello, World!")
 }
 say.hello()
 #
 # One argument --------------------------------------- #
 sprintf("Hello %s", "John")
-hello.person <- function(name)
-{
-  print(sprintf("Hello %s", name))
+hello.person <- function(name) {
+  print( sprintf("Hello %s", name) )
 }
 hello.person("John")
 hello.person("Mary")
+# View(hello.person)
+# edit(hello.person)
 #
 # two arguments? --------------------------------------- #
 sprintf("Hello %s, today is %s", "John", "Friday")
-hello.person <- function(first, last)
-{
+hello.person <- function(first, last) {
   print(sprintf("Hello %s, today is %s", first, last))
 }
 hello.person("Jane", "Monday")
 #
-# ------------< HW1: upload a useful function written by yourself -------------
-### define a function with an input vector for a purpose
+# ------------------< HW1: write a useful function by yourself -------------
+#
+# Define a function with two input vectors and one output vector for a purpose
 #
